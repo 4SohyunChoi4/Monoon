9bddeb33e0e51e143c33b8829af6bdc22643b30b
KYJ0314 <rladuswl01@sookmyung.ac.kr>
2021-04-01 16:41:13 +0900

    UnityMultiPlayerTutorial
    UnityMultiPlayerTutorial

---
+++ UnityMultiplayerTutorial/Assets/MenuController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private string VersionName = "0.1";
+    [SerializeField] private GameObject UsernameMenu;
+    [SerializeField] private GameObject ConnectPanel;
+
+    [SerializeField] private InputField UsernameInput;
+    [SerializeField] private GameObject StartButton;
+
+    void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersionName);
+    }
+
+    private void Start()
+    {
+        UsernameMenu.SetActive(true);
+    }
+
+
+    private void OnConnectedToMaster() 
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+    public void ChanseUserNameInput()
+    {
+        if (UsernameInput.text.Length >= 3)
+        {
+            StartButton.SetActive(true);
+        }
+        else
+        {
+            StartButton.SetActive(false);
+        }
+    }
+
+    public void SetUserName()
+    {
+        UsernameMenu.SetActive(false);
+        PhotonNetwork.playerName = UsernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom("광장", new RoomOptions() { maxPlayers = 5 }, null);
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.maxPlayers = 5;
+        PhotonNetwork.JoinOrCreateRoom("광장", roomOptions, TypedLobby.Default);
+    }
+
+    private void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MainGame");
+    }
+}
---
+++ UnityMultiplayerTutorial/Assets/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : Photon.MonoBehaviour
+{
+    public PhotonView photonView;
+    public Rigidbody2D rb;
+    //public Rigidbody rb;
+    public Animator anim;
+    //public GameObject playerCamera;
+    public SpriteRenderer sr;
+    public Text PlayerNameText;
+
+    public bool IsGrounded = false;
+    public float MoveSpeed;
+    public float JumpForce;
+
+    private void Awake()
+    {
+        if (photonView.isMine)
+        {
+            PlayerNameText.text = PhotonNetwork.playerName;
+        }
+
+        else
+        {
+            PlayerNameText.text = photonView.owner.name;
+            PlayerNameText.color = Color.cyan;
+        }
+
+    }
+
+    private void Update()
+    {
+        if (photonView.isMine)
+        {
+            CheckInput();
+        }
+    }
+
+    private void CheckInput()
+    {
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        transform.position += move * MoveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            photonView.RPC("FlipXTrue", PhotonTargets.AllBuffered);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            photonView.RPC("FlipXFalse", PhotonTargets.AllBuffered);
+        }
+
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow)) || ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))))
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+    }
+
+    [PunRPC]
+    private void FlipXTrue()
+    {
+        sr.flipX = true;
+    }
+
+    [PunRPC]
+    private void FlipXFalse()
+    {
+        sr.flipX = false;
+    }
+}
