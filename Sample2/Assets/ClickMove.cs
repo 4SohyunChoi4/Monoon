a2950bbcd5c91229fb0961f54cf926f5b857285e
moldParfait <snowgg0500@sookmyung.ac.kr>
2021-04-06 16:08:01 +0900

    touchmove

---
+++ Sample2/Assets/ClickMove.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickMove : MonoBehaviour
+{
+
+    private Camera camera;
+    private Animator animator;
+
+    private bool isMove;
+    private Vector3 destination;
+    private void Awake()
+    {
+        camera = Camera.main;
+        animator = GetComponentInChildren<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                SetDestination(hit.point);
+            }
+        }
+
+        Move();
+    }
+
+    private void SetDestination(Vector3 dest)
+    {
+        destination = dest;
+        isMove = true;
+        animator.SetBool("isMove", true);
+    }
+
+    private void Move()
+    {
+        if (isMove)
+        {
+            var dir = destination - transform.position;
+            transform.forward = dir;
+            transform.position += dir.normalized * Time.deltaTime * 5f;
+        }
+
+        if (Vector3.Distance(transform.position, destination) <= 0.1f)
+        {
+            isMove = false;
+            animator.SetBool("isMove", false);
+        }
+    }
+}
